5691d9e08540c5b0897d167cdb863c4f60eec6df
Dann Mitchell <dm16883@bc4login2.bc4.acrc.priv>
2018-11-07 10:18:59 +0000

    Student directory start

---
+++ exp/test_cases/held_suarez/run-bc_test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --job-name=test_planet
+#SBATCH --partition=serial
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=12
+#SBATCH --cpus-per-task=1
+#SBATCH --time=6:00:00
+#SBATCH --mem=20G
+
+module load build/gcc-7.2.0
+module load languages/anaconda2/5.0.1
+module load tools/git/2.18.0
+
+echo 'modules loaded'
+module list
+
+source activate isca_env
+
+time python held_suarez_test_case.py
